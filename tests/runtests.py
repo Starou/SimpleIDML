e9aa0dadbe84944d7bcdeee2a1417716f47c1332
Stanislas <stanislas.guerra@gmail.com>
2012-03-26 11:23:06 +0200

    Add the possibility in runtests.py to play a single method.

--- tests/runtests.py
+++ tests/runtests.py
@@ -10,7 +10,8 @@ sys.path.insert(0,'../src/')
 def load_suite_tests(only=None):
     only_module, only_test_case = None, None
     if only:
-        only_module, only_test_case = only.split(".")
+        args = only.split(".")
+        only_module, only_test_case, only_function = args[0], args[1], (args[2:] and args[2] or None)
     suites = []
     for dirpath, dirnames, filenames in os.walk(REGRESSION_TEST_DIR):
         for f in filenames:
@@ -23,6 +24,8 @@ def load_suite_tests(only=None):
                     suite = mod.suite()
                     if only_test_case:
                         suite._tests = [t for t in suite._tests if t.__class__.__name__ == only_test_case]
+                        if only_function:
+                            suite._tests = [t for t in suite._tests if t._testMethodName == only_function]
                     suites.append(suite)
     return suites
 
