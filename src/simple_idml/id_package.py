4721351cc70b46f7c0876a08215660fe6626f0fe
Stanislas <stanislas.guerra@gmail.com>
2012-07-25 14:34:48 +0200

    Add a new class ZipInDesignPackage to manage zipped packages.

---
+++ src/simple_idml/id_package.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+import zipfile
+import re
+
+DOCUMENT_FONT_DIR = "Document fonts"
+DOCUMENT_LINK_DIR = "Links"
+ADOBE_FONT_LIST = "AdobeFnt13.lst"
+
+class ZipInDesignPackage(zipfile.ZipFile):
+    """ Wrapper over a zipped InDesign Package. 
+    
+       The Package is obtained with the action `File > Package' from InDesign menu. """
+
+    rx_font = re.compile("(?P<root_dir>.*)/%s/(?P<font_name>.*)" % DOCUMENT_FONT_DIR)
+    rx_link = re.compile("(?P<root_dir>.*)/%s/(?P<link_name>.*)" % DOCUMENT_LINK_DIR)
+
+    def get_font_list(self):
+        return [(self.rx_font.match(filename).groupdict()["font_name"], filename)\
+         for filename in self.namelist() if self.rx_font.match(filename)]
+
+    def get_link_list(self):
+        return [(self.rx_link.match(filename).groupdict()["link_name"], filename)\
+         for filename in self.namelist() if self.rx_link.match(filename)]
