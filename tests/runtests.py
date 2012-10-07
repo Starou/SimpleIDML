dcfebd7f5c585bf6924f2abb5b6b3981fa44b41b
Stanislas <stanislas.guerra@gmail.com>
2012-10-07 21:09:52 +0200

    PEP8 runtests.py.

--- tests/runtests.py
+++ tests/runtests.py
@@ -1,17 +1,20 @@
+import os
+import sys
 import unittest
-import os, sys
 
-REGRESSION_TEST_DIRNAME='regressiontests'
+REGRESSION_TEST_DIRNAME = 'regressiontests'
 REGRESSION_TEST_DIR = os.path.join(os.path.dirname(__file__), REGRESSION_TEST_DIRNAME)
 
-sys.path.insert(0,'../src/')
+sys.path.insert(0, '../src/')
 
 
 def load_suite_tests(only=None):
     only_module, only_test_case = None, None
     if only:
         args = only.split(".")
-        only_module, only_test_case, only_function = args[0], (args[1:] and args[1] or None), (args[2:] and args[2] or None)
+        only_module = args[0]
+        only_test_case = args[1:] and args[1] or None
+        only_function = args[2:] and args[2] or None
     suites = []
     for dirpath, dirnames, filenames in os.walk(REGRESSION_TEST_DIR):
         for f in filenames:
@@ -23,9 +26,11 @@ def load_suite_tests(only=None):
                 if hasattr(mod, 'suite'):
                     suite = mod.suite()
                     if only_test_case:
-                        suite._tests = [t for t in suite._tests if t.__class__.__name__ == only_test_case]
+                        suite._tests = [t for t in suite._tests
+                                        if t.__class__.__name__ == only_test_case]
                         if only_function:
-                            suite._tests = [t for t in suite._tests if t._testMethodName == only_function]
+                            suite._tests = [t for t in suite._tests
+                                            if t._testMethodName == only_function]
                     suites.append(suite)
     return suites
 
