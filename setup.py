0c9f89b5775575eab1fc5f09f50a7fdeff34ba71
Stanislas Guerra <stanislas.guerra@gmail.com>
2014-09-10 13:31:47 +0200

    Bump to version 0.91.4 since MANIFEST.in was not referencing README.rst. Weird...

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name="SimpleIDML",
-    version="0.91.3",
+    version="0.91.4",
     license='BSD Licence',
     author='Stanislas Guerra',
     author_email='stanislas.guerra@gmail.com',
