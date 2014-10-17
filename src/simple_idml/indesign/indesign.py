f26877048ced471cf0f409cdd7600440fafec689
Stanislas Guerra <stanislas.guerra@gmail.com>
2014-10-17 15:49:48 +0200

    Fix indesign.save_as() with the zip parameter.

--- src/simple_idml/indesign/indesign.py
+++ src/simple_idml/indesign/indesign.py
@@ -1,15 +1,15 @@
 # -*- coding: utf-8 -*-
 
+import ftplib
 import ntpath
 import os
 import shlex
 import shutil
 import subprocess
 import zipfile
-from ftplib import FTP
 from io import BytesIO
 from suds.client import Client
-from tempfile import mkstemp
+from tempfile import mkdtemp
 
 CURRENT_DIR = os.path.abspath(os.path.split(__file__)[0])
 SCRIPTS_DIR = os.path.join(CURRENT_DIR, "scripts")
@@ -106,7 +106,7 @@ def _copy(src_filename, dst_filename, ftp_params=None):
         shutil.copy(src_filename, dst_filename)
         return
     with open(src_filename, "rb") as f:
-        ftp = FTP(*ftp_params["auth"])
+        ftp = ftplib.FTP(*ftp_params["auth"])
         ftp.set_pasv(ftp_params["passive"])
         try:
             ftp.storbinary('STOR %s' % dst_filename, f)
@@ -122,7 +122,7 @@ def _unlink(filename, ftp_params=None):
     if not ftp_params:
         os.unlink(filename)
         return
-    ftp = FTP(*ftp_params["auth"])
+    ftp = ftplib.FTP(*ftp_params["auth"])
     ftp.set_pasv(ftp_params["passive"])
     ftp.delete(filename)
     ftp.quit()
@@ -132,9 +132,9 @@ def _rmtree(tree, ftp_params=None):
     if not ftp_params:
         shutil.rmtree(tree)
         return
-    ftp = FTP(*ftp_params["auth"])
+    ftp = ftplib.FTP(*ftp_params["auth"])
     ftp.set_pasv(ftp_params["passive"])
-    ftp.rmd(tree)
+    rmtree_ftp(ftp, tree)
     ftp.quit()
 
 
@@ -146,7 +146,7 @@ def _read(filename, ftp_params=None):
             response = f.read()
     else:
         with BytesIO() as r:
-            ftp = FTP(*ftp_params["auth"])
+            ftp = ftplib.FTP(*ftp_params["auth"])
             ftp.set_pasv(ftp_params["passive"])
             ftp.retrbinary('RETR %s' % filename, r.write)
             ftp.quit()
@@ -160,23 +160,27 @@ def _zip_dir(dirname, zip_filename, ftp_params=None):
     if ftp_params:
         # Work locally in a temporary directory.
         # and then upload the zip to the ftp.
-        ftp_dirname = dirname
-        ftp_zip_filename = zip_filename
-        dirname = mkstemp()[1]
-        zip_filename = os.path.join(dirname, os.path.basename(ftp_zip_filename))
-        cmd = 'wget -r --no-passive-ftp --ftp-user="%(user)s" --ftp-password="%(passwd)s" %(url)s/%(ftp_dirname)s -P %(dst)s -nH -q' % {
-            'user': ftp_params[1],
-            'passwd': ftp_params[2],
-            'url': ftp_params[0],
-            'ftp_dirname': ftp_dirname,
-            'dst': dirname
-        }
-        subprocess.Popen(shlex.split(cmd))
-        _copy(zip_filename, ftp_zip_filename, ftp_params)
-        _rmtree(ftp_dirname, ftp_params)
+        tmp_dirname = mkdtemp()
+        tmp_zip_filename = os.path.join(tmp_dirname, os.path.basename(zip_filename))
 
-    zip_tree(dirname, zip_filename)
-    shutil.rmtree(dirname)
+        cmd = 'wget -r %(no_passive)s --ftp-user="%(user)s" --ftp-password="%(passwd)s" ftp://%(url)s/%(dirname)s -P %(dst)s -nH -nv' % {
+            'user': ftp_params['auth'][1],
+            'passwd': ftp_params['auth'][2],
+            'url': ftp_params['auth'][0],
+            'no_passive': ftp_params['passive'] and "" or "--no-passive-ftp",
+            'dirname': dirname,
+            'dst': tmp_dirname
+        }
+        p = subprocess.Popen(shlex.split(cmd))
+        p.wait()
+
+        zip_tree(os.path.join(tmp_dirname, dirname), tmp_zip_filename)
+        _copy(tmp_zip_filename, zip_filename, ftp_params)
+        shutil.rmtree(tmp_dirname)
+        _rmtree(dirname, ftp_params)
+    else:
+        zip_tree(dirname, zip_filename)
+        shutil.rmtree(dirname)
 
 
 def zip_tree(tree, destination):
@@ -191,3 +195,33 @@ def zip_tree(tree, destination):
                 if os.path.isfile(filename):  # regular files only
                     arcname = os.path.join(os.path.relpath(root, relroot), file)
                     zip.write(filename, arcname)
+
+
+# https://gist.github.com/Starou/beb8bde114bf7a20cf80
+def rmtree_ftp(ftp, path):
+    """Recursively delete a directory tree on a remote server."""
+    wd = ftp.pwd()
+
+    try:
+        names = ftp.nlst(path)
+    except ftplib.all_errors as e:
+        # some FTP servers complain when you try and list non-existent paths
+        #_log.debug('FtpRmTree: Could not remove {0}: {1}'.format(path, e))
+        return
+
+    for name in names:
+        if os.path.split(name)[1] in ('.', '..'):
+            continue
+
+        try:
+            ftp.cwd(name)  # if we can cwd to it, it's a folder
+            ftp.cwd(wd)  # don't try a nuke a folder we're in
+            rmtree_ftp(ftp, name)
+        except ftplib.all_errors:
+            ftp.delete(name)
+
+    try:
+        ftp.rmd(path)
+    except ftplib.all_errors as e:
+        raise e
+
