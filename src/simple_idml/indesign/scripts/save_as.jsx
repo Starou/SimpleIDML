551d361fdb5d8caf0373f3034afdbc95757744a5
Stanislas Guerra <stanislas.guerra@gmail.com>
2014-10-22 16:48:32 +0200

    save_as.jsx : interactive select of in and out files for execution via ExtendScript.

--- src/simple_idml/indesign/scripts/save_as.jsx
+++ src/simple_idml/indesign/scripts/save_as.jsx
@@ -1,8 +1,12 @@
-var src_filename = app.scriptArgs.get("source");
-var dst_filename = app.scriptArgs.get("destination");
-app.open(File(src_filename));
+if (!app.scriptArgs.isDefined("source")) {
+    var src_file = File.openDialog("Choose the source file");
+    var dst_file = File.saveDialog("Choose the destination file");
+} else {
+    var src_file = new File(app.scriptArgs.get("source"));
+    var dst_file = new File(app.scriptArgs.get("destination"));
+}
 
+app.open(src_file, false);
 var myDocument = app.documents.item(0);
-myDocument.save(new File(dst_filename));
-
+myDocument.save(dst_file);
 app.documents.item(0).close();
