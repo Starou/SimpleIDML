96c5a35fea03e8b73c3c892a7ed03a1ad03f6943
Stanislas Guerra <sguerra@figarocms.fr>
2017-11-23 16:37:59 +0100

    create_idml_package_from_dir: check that directory exist.

--- src/simple_idml/extras.py
+++ src/simple_idml/extras.py
@@ -5,8 +5,10 @@ from simple_idml.idml import IDMLPackage
 
 
 def create_idml_package_from_dir(src_dir, destination):
+    if not os.path.exists(src_dir):
+        raise IOError("%s does not exist." % src_dir)
     if os.path.exists(destination):
-        raise IOError("%s already exists." % destination)
+        raise IOError("%s already exist." % destination)
 
     package = IDMLPackage(destination, mode="w")
 
