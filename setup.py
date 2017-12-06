cfde58e334cdb2872291648918830eb9918fe479
Stanislas Guerra <sguerra@figarocms.fr>
2017-12-06 11:13:45 +0100

    Bump version to 1.0.0, Add Python 3.5 & 3.6 to CI.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name="SimpleIDML",
-    version="0.92.9",
+    version="1.0.0",
     license='BSD Licence',
     author='Stanislas Guerra',
     author_email='stanislas.guerra@gmail.com',
