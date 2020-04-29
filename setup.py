1257ee6e32b23e7f7b863e1cd30d6532ec3bbcda
Stanislas Guerra <stan@slashdev.fr>
2020-04-29 15:52:31 +0200

    Remove Python2 in packaging.

--- setup.py
+++ setup.py
@@ -1,20 +1,11 @@
 import os
-import sys
 from setuptools import setup
-
-# Python 2.7
-from io import open
 
 with open(os.path.join(os.path.dirname(__file__), 'README.rst'), encoding='utf-8') as f:
     README = f.read()
 
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
-
-if sys.version_info.major == 3:
-    suds_package = "suds-py3"
-else:
-    suds_package = "suds"
 
 setup(
     name="SimpleIDML",
@@ -30,7 +21,7 @@ setup(
         'Issue Tracker': 'https://github.com/Starou/SimpleIDML/issues',
     },
     package_dir={'': 'src'},
-    install_requires=['future', 'lxml', suds_package],
+    install_requires=['future', 'lxml', 'suds-py3'],
     packages=[
         'simple_idml',
         'simple_idml.indesign',
@@ -52,7 +43,6 @@ setup(
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
