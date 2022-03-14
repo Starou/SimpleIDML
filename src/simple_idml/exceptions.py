5faff8fd62f23867fce0b3868864590c10a0aa18
Stanislas Guerra <stan@slashdev.fr>
2022-03-14 17:04:44 +0100

    exceptions.py: PyLint.

--- src/simple_idml/exceptions.py
+++ src/simple_idml/exceptions.py
@@ -2,7 +2,7 @@ class InDesignSoapException(BaseException):
     _error = None
 
     def __init__(self, script_params, response):
-        self._error = "Script params: %s\nResponse:  %s" % (script_params, response)
+        self._error = f"Script params: {script_params}\nResponse: {response}"
 
     def __str__(self):
         return repr(self._error)
