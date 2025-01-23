2d4f4fe88b92e275295a0615128056fe424f4ed5
Stanislas Guerra <stan@slashdev.fr>
2025-01-23 15:30:40 +0100

    Bump to 1.2.0.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name="SimpleIDML",
-    version="1.1.8a3",
+    version="1.2.0",
     license='BSD Licence',
     author='Stanislas Guerra',
     author_email='stanislas.guerra@gmail.com',
